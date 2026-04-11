271705c8ea77b72a53075791346020c198c2137a
ismia-shin <islamsamiha700@gmail.com>
2026-04-11 23:25:16 +0600

    Recreate database structure for studydock

--- database/studydock.sql
+++ database/studydock.sql
@@ -20,6 +20,9 @@ SET time_zone = "+00:00";
 --
 -- Database: `studydock`
 --
+DROP DATABASE IF EXISTS studydock;
+CREATE DATABASE IF NOT EXISTS studydock;
+USE studydock;
 
 -- --------------------------------------------------------
 
